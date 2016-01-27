7df02b546b9815eee9abb40a596d5b65a5763b7d
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-01-27 13:32:51 -0500

    Predict technology

---
+++ detect_technology.js
@@ -0,0 +1,35 @@
+var GitUrlParse = require("git-url-parse");	//sudo npm i --save git-url-parse
+var Git = require("nodegit");	//sudo npm install nodegit --save
+var exec = require('child_process').exec;
+var spawn = require('child_process').spawn;
+var child;
+
+/*var name_github_repo = GitUrlParse("git@github.com:mrajashree/linguist.git").name;
+git@github.com:mrajashree/App.git
+console.log("Name of github repo : ", name_github_repo);*/
+var url_github_repo = process.argv[2];
+var name_github_repo = GitUrlParse(url_github_repo).name;
+
+var py =
+{
+  detect: function()
+  {
+    var command = 'pylinguist '+name_github_repo;
+    exec(command, function(err,out,code)
+    {
+      console.log("Technology of the repo is: ",out.split('\n')[0].split(' ')[1]);
+      //console.log("Technology of the repo is: ",out);
+      process.exit();
+    });
+    
+	}
+}
+
+
+Git.Clone(url_github_repo, name_github_repo)
+	.then(function(repository) {console.log("Cloned and repo created");})
+	.then(function() {py.detect();})
+	.done(function () { console.log("Done!");  });
+
+console.log("Name of github repo : ", name_github_repo);
+
