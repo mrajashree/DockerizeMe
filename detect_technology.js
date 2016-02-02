3f059fcaa461951ffb2169e2ab1f5e56b6517584
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-02-01 19:09:28 -0500

    git clone using exec

--- detect_technology.js
+++ detect_technology.js
@@ -1,14 +1,24 @@
-var GitUrlParse = require("git-url-parse");	//sudo npm i --save git-url-parse
-var Git = require("nodegit");	//sudo npm install nodegit --save
+var GitUrlParse = require("git-url-parse"); //sudo npm i --save git-url-parse
+var Git = require("nodegit"); //sudo npm install nodegit --save
 var exec = require('child_process').exec;
-var spawn = require('child_process').spawn;
 var child;
 
+/*var name_github_repo = GitUrlParse("git@github.com:mrajashree/linguist.git").name;
+git@github.com:mrajashree/App.git
+console.log("Name of github repo : ", name_github_repo);*/
 var url_github_repo = process.argv[2];
 var name_github_repo = GitUrlParse(url_github_repo).name;
 
 var py =
 {
+  clone: function()
+  {
+    var clone_cmd = 'git clone '+url_github_repo+' '+name_github_repo;
+    exec(clone_cmd, function(err,out,code) {
+      py.detect();
+    })
+  },
+
   detect: function()
   {
     var command = 'pylinguist '+name_github_repo;
@@ -19,14 +29,14 @@ var py =
       process.exit();
     });
     
-	}
+  }
 }
 
 
-Git.Clone(url_github_repo, name_github_repo)
-	.then(function(repository) {console.log("Cloned and repo created");})
-	.then(function() {py.detect();})
-	.done(function () { console.log("Done!");  });
-
+/*Git.Clone(url_github_repo, name_github_repo)
+  .then(function(repository) {console.log("Cloned and repo created");})
+  .then(function() {py.detect();})
+  .done(function () { console.log("Done!");  });*/
+py.clone();
 console.log("Name of github repo : ", name_github_repo);
 
