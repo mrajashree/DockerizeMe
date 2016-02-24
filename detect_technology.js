a36a1f54a2af7fee6d5bacb169bd382d4754aea2
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-02-24 14:17:06 -0500

    predict technology

--- detect_technology.js
+++ detect_technology.js
@@ -1,10 +1,6 @@
 var GitUrlParse = require("git-url-parse"); //sudo npm i --save git-url-parse
 var exec = require('child_process').exec;
 var child;
-
-/*var name_github_repo = GitUrlParse("git@github.com:mrajashree/linguist.git").name;
-git@github.com:mrajashree/App.git
-console.log("Name of github repo : ", name_github_repo);*/
 var url_github_repo = process.argv[2];
 var name_github_repo = GitUrlParse(url_github_repo).name;
 
@@ -31,11 +27,6 @@ var py =
   }
 }
 
-
-/*Git.Clone(url_github_repo, name_github_repo)
-  .then(function(repository) {console.log("Cloned and repo created");})
-  .then(function() {py.detect();})
-  .done(function () { console.log("Done!");  });*/
 py.clone();
 console.log("Name of github repo : ", name_github_repo);
 
