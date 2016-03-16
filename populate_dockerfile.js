c94a1370c4cbe553cbb6514f0d6960222a3e158c
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-03-16 11:03:24 -0400

    Updated

--- populate_dockerfile.js
+++ populate_dockerfile.js
@@ -1,46 +1,65 @@
 var fs = require('fs');
+var technology, path_repo;
+var pckgjson = require('./package.json');
 
 var builder = {};
 builder.basedependencies = {
-	"Java":"maven",
-	"Javascript":"node.js",
+	"Java":"jdk",
+	"JavaScript":"node.js",
 	"Python":"python"
 }
 
 builder.packagemanager = {
-	"Javascript":"npm"
+	"JavaScript":"npm",
+	"Java":"maven",
+	"Python":"pip_requirements"
 }
 
 builder.packagedependencies = {
 	"Java":"pom.xml",
-	"Javascript":"package.json",
+	"JavaScript":"package.json",
 	"Python":"requirements.txt"
 }
 
 builder.baseinstallcommands = {
-	"maven":"RUN sudo apt-get install openjdk-7-jdk\nRUN wget http://www.eng.lsu.edu/mirrors/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.zip\nRUN unzip apache-maven-3.2.3-bin.zip",
-	"node.js":"RUN curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -\nRUN sudo apt-get install -y nodejs",
-	"python":"RUN sudo apt-get install -y python2.7\nRUN sudo apt-get install -y python-pip"
+	"jdk":"RUN sudo apt-get install openjdk-7-jdk\n",
+	"node.js":"RUN sudo apt-get install -y nodejs\n",
+	"python":"RUN sudo apt-get install -y python2.7\nRUN sudo apt-get install -y python-pip\n"
 }
 
 builder.packageinstallcommands = {
-	"npm":"RUN sudo apt-get install -y npm\nRUN npm install"
+	"npm":"RUN sudo apt-get install -y npm\nCOPY . /src\nRUN cd /src;sudo npm install\n",
+	"maven":"RUN wget http://www.eng.lsu.edu/mirrors/apache/maven/maven-3/3.2.3/binaries/apache-maven-3.2.3-bin.zip\nRUN unzip apache-maven-3.2.3-bin.zip\n",
+	"pip_requirements":"COPY . /src\nRUN cd /src;pip install -r requirements.txt\n"
 }
 
-var technology = process.argv[2];
 
-console.log(builder.baseinstallcommands[builder.basedependencies[technology]]);	//command(s) to install basedependency
-
-
-var path_repo = process.argv[3];	
-
-fs.exists('./'+path_repo+"/"+builder.packagedependencies[technology], function(exists) {
-	if(exists) {
-		console.log("Requirements met");
-		if(builder.packagemanager[technology])
-		{
-			console.log(builder.packageinstallcommands[builder.packagemanager[technology]])
-		} 
-	}
+fs.readFile('op.txt', 'utf8',function (err, data) {
+  if (err) throw err;
+  path_repo = data.trim().split(" ")[0];
+  technology = data.trim().split(" ")[1];
+  console.log("FROM ubuntu:14.04\n\nRUN sudo apt-get update\n")
+  populate(path_repo,technology);
 });
+
+// technology = process.argv[3];
+// path_repo = process.argv[2];
+
+function populate(path_repo,technology) {
+	console.log(builder.baseinstallcommands[builder.basedependencies[technology]]);	//command(s) to install basedependency
+
+	fs.exists('./'+path_repo+"/"+builder.packagedependencies[technology], function(exists) {
+		if(exists) {
+			//console.log("Requirements met");
+			if(builder.packagemanager[technology])
+			{
+				console.log(builder.packageinstallcommands[builder.packagemanager[technology]])
+			} 
+		}
+		else
+		{
+			console.log("Package manager not found");
+		}
+	});
+}
 
