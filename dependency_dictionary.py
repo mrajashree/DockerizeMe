be21957888c22c0b1529b64ffd2739fffe5b6e8d
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-04-10 17:41:44 -0400

    Updated dictionary

--- dependency_dictionary.py
+++ dependency_dictionary.py
@@ -1,7 +1,7 @@
 #scan requirements.txt
 
 requirements_dictionary = {}
-requirements_dictionary['Pygments'] = ['libmagic-dev','libicu-dev']
+requirements_dictionary['Pygments'] = ['libmagic-dev','libicu-dev','python-pygments']
 requirements_dictionary['git-url-parse'] = ['git']
 requirements_dictionary['node-icu-charset-detector'] = ['libmagic-dev','libicu-dev']
 requirements_dictionary['filemagic'] = ['libmagic-dev']
