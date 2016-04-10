fedebbb9caa5c3683a5c90822208e93b74b1fc8c
Rajashree Mandaogane <rajashree.28m@gmail.com>
2016-04-10 16:14:45 -0400

    added more packages

--- dependency_dictionary.py
+++ dependency_dictionary.py
@@ -2,3 +2,7 @@
 
 requirements_dictionary = {}
 requirements_dictionary['Pygments'] = ['libmagic-dev','libicu-dev']
+requirements_dictionary['git-url-parse'] = ['git']
+requirements_dictionary['node-icu-charset-detector'] = ['libmagic-dev','libicu-dev']
+requirements_dictionary['filemagic'] = ['libmagic-dev']
+requirements_dictionary['wxPython'] = ['build-essential','libgtk2.0-dev','gconf']
